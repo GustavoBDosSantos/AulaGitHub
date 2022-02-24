44b07ef0932d59835d386797eb6c51eb46029778
Gustavo B. Dos Santos <gustavo.b2005@aluno.ifsc.edu.br>
2022-02-24 17:03:50 -0300

    Inicio do projeto - Imprimindo meu nome com a classe Scanner

---
+++ src/githubgustavo.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IFSC
+ */
+public class githubgustavo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner leia = new Scanner (System.in);
+            System.out.println ("Entre com seu nome");
+            String nome = leia.nextLine () ;
+            System.out.println (nome+" Bem vindo ao GitHub !");
+              
+
+}
+}
